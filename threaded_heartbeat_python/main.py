a853084a9345145e968cd7a338ce8c48106472a4
N.T. Khan <khan@NTs-MacBook-Pro.local>
2024-10-17 14:58:34 -0600

    use with threadpool, properly shutdown heartbeat thread, rm extra interceptors, use setattr instead of wrap

--- threaded_heartbeat_python/main.py
+++ threaded_heartbeat_python/main.py
@@ -6,14 +6,9 @@ from contextvars import copy_context
 from datetime import datetime, timedelta
 from typing import Any, Type, TypeVar, Callable, Awaitable, cast
 from concurrent.futures import ThreadPoolExecutor
-import temporalio.activity
-import temporalio.exceptions
 import asyncio
 import threading
-import temporalio.api.common.v1
-import temporalio.client
 import temporalio.converter
-import temporalio.worker
 import temporalio.workflow
 import time
 
@@ -22,37 +17,8 @@ F = TypeVar("F", bound=Callable[..., Awaitable[Any]])
 
 
 def auto_heartbeat(fn: F) -> F:
-    def wrapper(*args, **kwargs):
-        return fn(*args, **kwargs)
-    return cast(F, wrapper)
-
-
-class _WorkflowInboundInterceptor(
-    temporalio.worker.WorkflowInboundInterceptor
-):
-    def init(self, outbound: temporalio.worker.WorkflowOutboundInterceptor) -> None:
-        self.next.init(_WorkflowOutboundInterceptor(outbound))
-
-    async def execute_workflow(
-        self, input: temporalio.worker.ExecuteWorkflowInput
-    ) -> Any:
-        return await self.next.execute_workflow(input)
-
-    async def handle_signal(self, input: temporalio.worker.HandleSignalInput) -> None:
-        return await self.next.handle_signal(input)
-
-    async def handle_query(self, input: temporalio.worker.HandleQueryInput) -> Any:
-        return await self.next.handle_query(input)
-
-    def handle_update_validator(
-        self, input: temporalio.worker.HandleUpdateInput
-    ) -> None:
-        self.next.handle_update_validator(input)
-
-    async def handle_update_handler(
-        self, input: temporalio.worker.HandleUpdateInput
-    ) -> Any:
-        return await self.next.handle_update_handler(input)
+    setattr(fn, "__temporal_auto_heartbeat", True)
+    return fn
 
 
 class Interceptor(
@@ -68,54 +34,10 @@ class Interceptor(
         self._payload_converter = payload_converter
         self._executor = executor
 
-    def intercept_client(
-        self, next: temporalio.client.OutboundInterceptor
-    ) -> temporalio.client.OutboundInterceptor:
-        return _ClientOutboundInterceptor(
-            next, self._payload_converter
-        )
-
     def intercept_activity(
         self, next: temporalio.worker.ActivityInboundInterceptor
     ) -> temporalio.worker.ActivityInboundInterceptor:
         return _ActivityInboundInterceptor(next, self._executor)
-
-    def workflow_interceptor_class(
-        self, input: temporalio.worker.WorkflowInterceptorClassInput
-    ) -> Type[_WorkflowInboundInterceptor]:
-        return _WorkflowInboundInterceptor
-
-
-class _ClientOutboundInterceptor(
-    temporalio.client.OutboundInterceptor
-):
-    def __init__(
-        self,
-        next: temporalio.client.OutboundInterceptor,
-        payload_converter: temporalio.converter.PayloadConverter,
-    ) -> None:
-        super().__init__(next)
-        self._payload_converter = payload_converter
-
-    async def start_workflow(
-        self, input: temporalio.client.StartWorkflowInput
-    ) -> temporalio.client.WorkflowHandle[Any, Any]:
-        return await super().start_workflow(input)
-
-    async def query_workflow(self, input: temporalio.client.QueryWorkflowInput) -> Any:
-        return await super().query_workflow(input)
-
-    async def signal_workflow(
-        self, input: temporalio.client.SignalWorkflowInput
-    ) -> None:
-        await super().signal_workflow(input)
-
-    async def start_workflow_update(
-        self, input: temporalio.client.StartWorkflowUpdateInput
-    ) -> temporalio.client.WorkflowUpdateHandle[Any]:
-        return await self.next.start_workflow_update(input)
-
-
 
 
 class _ActivityInboundInterceptor(
@@ -132,51 +54,23 @@ class _ActivityInboundInterceptor(
     async def execute_activity(
         self, input: temporalio.worker.ExecuteActivityInput
     ) -> Any:
-        guard_heartbeat = "auto_heartbeat" in input.fn.__qualname__
-        stop_heartbeat_event = None
         heartbeat_timeout = activity.info().heartbeat_timeout
-        if heartbeat_timeout and guard_heartbeat:
+        guard_heartbeat = getattr(input.fn, "__temporal_auto_heartbeat") and heartbeat_timeout
+        stop_heartbeat_event = None
+        heartbeat_future = None
+        if guard_heartbeat:
             stop_heartbeat_event = threading.Event()
             ctx = copy_context()
-            asyncio.get_event_loop().run_in_executor(self._executor, ctx.run, heartbeat_every, stop_heartbeat_event,
-                                                     heartbeat_timeout.total_seconds() / 2)
+            heartbeat_future = asyncio.get_event_loop().run_in_executor(self._executor, ctx.run, heartbeat_every,
+                                                                        stop_heartbeat_event,
+                                                                        heartbeat_timeout.total_seconds() / 2)
         try:
             return await self.next.execute_activity(input)
         finally:
             if guard_heartbeat:
-                print("stopping heartbeats") # For demonstration purposes only. Feel free to remove.
+                print("stopping heartbeats")  # For demonstration purposes only. Feel free to remove.
                 stop_heartbeat_event.set()
-
-
-
-
-class _WorkflowOutboundInterceptor(
-    temporalio.worker.WorkflowOutboundInterceptor
-):
-    async def signal_child_workflow(
-        self, input: temporalio.worker.SignalChildWorkflowInput
-    ) -> None:
-        return await self.next.signal_child_workflow(input)
-
-    async def signal_external_workflow(
-        self, input: temporalio.worker.SignalExternalWorkflowInput
-    ) -> None:
-        return await self.next.signal_external_workflow(input)
-
-    def start_activity(
-        self, input: temporalio.worker.StartActivityInput
-    ) -> temporalio.workflow.ActivityHandle:
-        return self.next.start_activity(input)
-
-    async def start_child_workflow(
-        self, input: temporalio.worker.StartChildWorkflowInput
-    ) -> temporalio.workflow.ChildWorkflowHandle:
-        return await self.next.start_child_workflow(input)
-
-    def start_local_activity(
-        self, input: temporalio.worker.StartLocalActivityInput
-    ) -> temporalio.workflow.ActivityHandle:
-        return self.next.start_local_activity(input)
+                await heartbeat_future
 
 
 def heartbeat_every(stop_event: threading.Event, delay: float, *details: Any) -> None:
@@ -187,7 +81,7 @@ def heartbeat_every(stop_event: threading.Event, delay: float, *details: Any) ->
         print(f"Heartbeat at {datetime.now()}")  # This is for demonstration purposes only. Feel free to remove.
         if stop_event.is_set():
             break
-    return
+    return None
 
 
 @activity.defn
@@ -227,42 +121,43 @@ interrupt_event = asyncio.Event()
 
 
 async def main():
-    # Connect client
-    executor = ThreadPoolExecutor(max_workers=5)
-    client = await Client.connect(
-        "localhost:7233",
-        # Use an interceptor to automatically heartbeat functions decoarted w/ autoheartbeat using threads
-        interceptors=[Interceptor(executor)],
-    )
-    # Run a worker for the workflow
-    async with Worker(
-        client,
-        task_queue="custom_decorator-task-queue",
-        activities=[run_forever_activity],
-        workflows=[WaitForCancelWorkflow],
-        # Synchronous activities are not allowed unless we provide some kind of
-        # executor. This same thread pool could be passed to multiple workers if
-        # desired.
-        activity_executor=executor,
-    ):
-        # Wait until interrupted
-        print("Activity Worker started, ctrl+c to exit")
-
-        handle = await client.start_workflow(
-            WaitForCancelWorkflow.run,
-            id=f"custom_decorator-workflow-id",
-            task_queue="custom_decorator-task-queue",
+    # Max workers needs to be at least 2x max concurrent activities executed since each activity may take up 2 threads
+    # This setting will bottleneck the total concurrency of the system if set too low.
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        client = await Client.connect(
+            "localhost:7233",
+            # Use an interceptor to automatically heartbeat functions decorated w/ @autoheartbeat using threads
+            interceptors=[Interceptor(executor)],
         )
+        # Run a worker for the workflow
+        async with Worker(
+            client,
+            task_queue="custom_decorator-task-queue",
+            activities=[run_forever_activity],
+            workflows=[WaitForCancelWorkflow],
+            # Synchronous activities are not allowed unless we provide some kind of
+            # executor. This same thread pool could be passed to multiple workers if
+            # desired.
+            activity_executor=executor,
+        ):
+            # Wait until interrupted
+            print("Activity Worker started, ctrl+c to exit")
 
-        print("Started workflow, waiting 5 seconds before cancelling")
-        await asyncio.sleep(5)
-        print("cancelling workflow")
-        await handle.cancel()
-        result = await handle.result()
-        print(f"Result: {result}")
+            handle = await client.start_workflow(
+                WaitForCancelWorkflow.run,
+                id=f"custom_decorator-workflow-id",
+                task_queue="custom_decorator-task-queue",
+            )
 
-        await interrupt_event.wait()
-        print("Shutting down")
+            print("Started workflow, waiting 5 seconds before cancelling")
+            await asyncio.sleep(5)
+            print("cancelling workflow")
+            await handle.cancel()
+            result = await handle.result()
+            print(f"Result: {result}")
+
+            await interrupt_event.wait()
+            print("Shutting down")
 
 if __name__ == "__main__":
     loop = asyncio.new_event_loop()
